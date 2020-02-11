6207d48f76474b52866a1f96480e65c5a347b440
Shafaqat Ali <shafaqat.ali@tabcorp.com.au>
2020-02-11 12:53:36 +1000

    Electron version of the dropdown added.

---
+++ webpack.react.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+const config = {
+  target: 'electron-renderer',
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [htmlPlugin],
+};
+
+module.exports = (env, argv) => {
+  return config;
+};
